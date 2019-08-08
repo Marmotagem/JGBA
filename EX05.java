11f715a03a274c8343d2bd2238b7aff209fd81e7
Vinicius Cambraia <53883408+ViniciusCambraia@users.noreply.github.com>
2019-08-07 22:36:30 -0300

    Update EX05.java

--- EX05.java
+++ EX05.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
 
 public class EX05 {
+
+	public static void main(String[] args) {
+	
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Conversão Metros para Centímetros ");
+		System.out.print(" Quantos metros?  ");
+        int metros = entrada.nextInt();
+        
+        	System.out.println(" " +metros+ "m são exatamente " +metros*100 +"cm.");
+		
+        	entrada.close();
+        	
+	}
 
 }
