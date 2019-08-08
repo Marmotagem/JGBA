19f44c02426b336163dd8fe0f7e530c81261d504
Vinicius Cambraia <53883408+ViniciusCambraia@users.noreply.github.com>
2019-08-07 22:36:55 -0300

    Update EX06.java

--- EX06.java
+++ EX06.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
 
-public class EX06 {
+public class Ex06 {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print(" Quanto é o raio do circulo? ");
+		float raio = entrada.nextFloat();
+		
+		float area = (float) (Math.PI*(raio*raio)); 
+		
+		System.out.print("A area desse circulo é de:" +area);
+
+		entrada.close();
+	}
 
 }
