3ada56080092e47df353f8ba308bddb1d7c1104b
Vinicius Cambraia <53883408+ViniciusCambraia@users.noreply.github.com>
2019-08-07 22:31:03 -0300

    Update EX02.java

--- EX02.java
+++ EX02.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
 
-public class EX02 {
+public class Ex02 {
+
+	public static void main(String[] args) {
+
+		
+	Scanner entrada = new Scanner(System.in);
+	
+	System.out.print("Informe um número: ");
+		int num1 = entrada.nextInt();
+		
+	System.out.print("O número informado foi: " +num1);
+
+	entrada.close();
+	
+	}
 
 }
