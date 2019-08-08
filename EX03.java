e0667a3105507becd3e52067af2127b8264ee805
Vinicius Cambraia <53883408+ViniciusCambraia@users.noreply.github.com>
2019-08-07 22:34:40 -0300

    Update EX03.java

--- EX03.java
+++ EX03.java
@@ -1,4 +1,21 @@
 
+import java.util.Scanner;
+
 public class EX03 {
+
+	public static void main(String[] args) {
+	
+	Scanner entrada = new Scanner(System.in);
+	
+	System.out.print("Informe um número: ");
+		int num1 = entrada.nextInt();
+
+	System.out.print("Informe um número: ");
+		int num2 = entrada.nextInt();
+	
+		System.out.println("A Soma dos números informados é: "+(num1+num2));
+	
+		entrada.close();
+	}
 
 }
