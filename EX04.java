e56dcbd08ab5123bcaed17e2744e205603fc83b8
Vinicius Cambraia <53883408+ViniciusCambraia@users.noreply.github.com>
2019-08-07 22:36:06 -0300

    Update EX04.java

--- EX04.java
+++ EX04.java
@@ -1,4 +1,27 @@
+import java.util.Scanner;
 
-public class EX04 {
+public class EX04{
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Nota 1: ");
+		float nota1 = entrada.nextFloat();
+	
+		System.out.print("Nota 2: ");
+		float nota2 = entrada.nextFloat();
+	
+		System.out.print("Nota 3: ");
+		float nota3 = entrada.nextFloat();
+		
+		System.out.print("Nota 4: ");
+		float nota4 = entrada.nextFloat();
+	
+		System.out.println("Sua média é: " +(nota1+nota2+nota3+nota4)/4);
+	
+		entrada.close();
+	
+	}
 
 }
